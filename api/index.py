f10b61170170568fd4a7415da2e394ce57b45e0d
Rajalakshmi12 <poorthi.laxmi@gmail.com>
2025-03-15 22:57:13 +0000

    tenth-commit-with-simple-get

--- api/index.py
+++ api/index.py
@@ -6,13 +6,5 @@ app = FastAPI()
 @app.get("/", response_class=HTMLResponse)
 async def read_root():
     return """
-    <html>
-        <head>
-            <title>Test FastAPI on Vercel</title>
-        </head>
-        <body>
-            <h1>Hello, World!</h1>
-            <p>Welcome to the FastAPI app deployed on Vercel.</p>
-        </body>
-    </html>
+    Yes, this is a FastAPI server.
     """
