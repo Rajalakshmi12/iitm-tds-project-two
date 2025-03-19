74c6bcdfe8869ca306c4fd909dd40d5bfeeae4ac
Rajalakshmi12 <poorthi.laxmi@gmail.com>
2025-03-19 20:23:32 +0000

    simple / 37

--- api/index.py
+++ api/index.py
@@ -5,12 +5,4 @@ app = FastAPI()
 
 @app.get("/", response_class=HTMLResponse)
 async def root():
-    return """
-    <html>
-        <head><title>FastAPI on Vercel</title></head>
-        <body>
-            <h1>🚀 FastAPI is Running on Vercel!</h1>
-            <p>If you see this message, your deployment was successful.</p>
-        </body>
-    </html>
-    """
+    return "yes"
