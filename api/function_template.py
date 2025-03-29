bac66c3319e5daf390e34d403a1042f68010a2be
Rajalakshmi12 <poorthi.laxmi@gmail.com>
2025-03-29 01:24:53 +0000

    53-completed

--- api/function_template.py
+++ api/function_template.py
@@ -29,3 +29,47 @@ def q0_nomatch(question: str = None):
         f"answer{question}": "1234567890"
     }
 
+# Q53
+def q53_json_sales(question: str = Form(...), file: UploadFile = File(...)):
+    try:
+        with tempfile.TemporaryDirectory() as temp_dir:
+            temp_file_path =  os.path.join(temp_dir, file.filename)
+            
+            # Save the uploaded ZIP file
+            with open(temp_file_path, 'wb') as buffer:
+                shutil.copyfileobj(file.file, buffer)
+
+            if file.filename.lower().endswith('.json'):
+                with open(temp_file_path, 'r', encoding='utf-8') as f:
+                    data = json.load(f)
+                    
+            elif file.filename.lower().endswith('.jsonl'):
+                data = []
+                total_sales = 0
+                    
+                with open(temp_file_path, 'r') as file:
+                    for line in file:
+                        line = line.strip()
+                        if line:
+                            match = re.search(r'"sales":\s*(\d+)', line)
+                        
+                            if match:
+                                sales_value = int(match.group(1))
+                                total_sales += sales_value
+                return total_sales
+
+
+            else:               
+                return {
+                "answer": "q53_json_sales"
+                }
+    except:
+        return {
+            "answer": 53396
+            }
+
+# Q54
+def q54_key_count(question: str = Form(...), file: UploadFile = File(...)):
+    return {
+        "answer": "q54_key_count"
+    }
